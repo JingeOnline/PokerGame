3dcbc096eed2360f682429dc59c7e313c9b64e2e
Jinge Zhang <jinge_windows@hotmail.com>
2022-04-20 13:49:33 +1000

    添加了格式化显示纸牌

--- PokerGame/Program.cs
+++ PokerGame/Program.cs
@@ -30,9 +30,9 @@ namespace PokerGame
                 Console.WriteLine("\nPlease input your player name:");
                 string username = Console.ReadLine();
                 Round round = new Round(username);
-                IEnumerable<string> playerNameList = round.Players.Select(x => x.Id + ": " + x.Name);
-                string names = string.Join("\n", playerNameList);
-                Console.WriteLine("\nPlayers:\n" + names);
+                IEnumerable<string> playerNameList = round.Players.Select(x => x.Id + "-" + x.Name);
+                string names = string.Join("\n\t", playerNameList);
+                Console.WriteLine("\nPlayers:\n\t" + names);
                 countDown();
                 showPlayersCards(round.Players);
             }
@@ -79,12 +79,65 @@ namespace PokerGame
             foreach(Player player in players)
             {
                 Console.WriteLine(player.Name + ":");
-                //下面两条语句比较高级，不需要理解，作用就是把玩家手中所有牌的Display属性，组合成一个字符串。
-                IEnumerable<string> cardsDisplay = player.Cards.Select(x => x.Display);
-                string cardsDisplayText = string.Join(",", cardsDisplay);
-                Console.WriteLine(cardsDisplayText);
+                
+                showCardsDisplayAndIdInColumn(player.Cards);
                 Console.WriteLine();
             }
+        }
+        /// <summary>
+        /// 把一组牌打印到控制台上，显示格式尽量好看一点
+        /// </summary>
+        /// <param name="cards"></param>
+        static void showCardsDisplayAndIdInColumn(IEnumerable<Card> cards)
+        {
+            //先从小到大排序
+            cards=cards.OrderBy(x => x.Num);
+            IEnumerable<string> cardsDisplay = cards.Select(x => x.Display);
+            List<string> formatedDisplays = new List<string>();
+
+            foreach (string s in cardsDisplay)
+            {
+                int widthGap = getTextWidthGap(s);
+                if (widthGap == 0)
+                {
+                    formatedDisplays.Add(string.Format("{0,-5}", s));
+                }
+                else if (widthGap == 1)
+                {
+                    formatedDisplays.Add(string.Format("{0,-4}", s));
+                }
+                else if (widthGap == 2)
+                {
+                    formatedDisplays.Add(string.Format("{0,-3}", s));
+                }
+            }
+            //下面两条语句比较高级，不需要理解，作用就是把玩家手中所有牌的Display属性，组合成一个字符串。
+            string cardsDisplayText = string.Join("|", formatedDisplays);
+            IEnumerable<string> cardsId = cards.Select(x => x.Id.ToString());
+            //格式化字符串宽度和对齐方式
+            cardsId = cardsId.Select(x => string.Format("{0,-5}", x));
+            string cardsIdText = string.Join("|", cardsId);
+
+            Console.WriteLine(cardsDisplayText);
+            Console.WriteLine(cardsIdText);
+        }
+
+        /// <summary>
+        /// 由于中文字符，西文字符，Emoji字符的宽度不一样，为了对齐文字，需要检测文字中有几个非英文的字符。
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static int getTextWidthGap(string text)
+        {
+            int width = 0;
+            foreach(char c in text)
+            {
+                if (c > 127)
+                {
+                    width++;
+                }
+            }
+            return width; 
         }
 
     }
---
+++ Test/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>()
+            {
+                "我",
+                "Mike",
+                "Jackson",
+                "你",
+                "测定",
+            };
+            List<string> ages = new List<string>()
+            {
+                "11",
+                "5",
+                "44",
+                "6",
+                "1589"
+            };
+
+            List<string> namesF = names.Select(x => string.Format("{0,-10}", x)).ToList();
+            List<string> agesF = ages.Select(x => string.Format("{0,-10}", x)).ToList();
+
+            //string s = string.Format("{0,-10}", "Hello");
+            //string t = string.Format("{0,-10}", "K");
+            Console.OutputEncoding= Encoding.UTF8;
+            Console.WriteLine(string.Join(",",namesF));
+            Console.WriteLine(string.Join(",",agesF));
+            Console.ReadKey();
+        }
+    }
+}
